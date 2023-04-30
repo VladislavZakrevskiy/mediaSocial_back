c372d930c67505d803a4c52c269b0fcc9bc1c771
vlad <zakrevskiyvlada@yandex.ru>
2023-04-30 19:09:53 +0400

    add db

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "postgres",
-    password: "1234",
-    host: "localhost",
+    user: "mediasocial_db_user",
+    password: "EVd35D9G1drRMGoMRACdIE85ofrlzap7",
+    host: "dpg-ch76g2jhp8u9bo5d11k0-a.frankfurt-postgres.render.com",
     port: 5432,
-    database:"vk-clone"
+    database:"mediasocial_db"
 })
  
 module.exports = pool
